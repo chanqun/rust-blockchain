e02d587206bf1cebf764253247bbc9f5f22ed828
chanqun <cksgns93@naver.com>
2021-11-14 13:20:52 +0900

    blocks & hashing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main () {
-    println!("Hello, blockchain!");
+use std::fmt::Debug;
+use blockchainlib::{Block, Hashable};
+
+fn main() {
+    let mut block = Block::new(0, 0, vec![0; 32], 0, "Genesis block".to_owned());
+
+    println!("{:?}", &block);
+
+    let h = block.hash();
+
+    println!("{:?}", &h);
+
+    block.hash = h;
+
+    println!("{:?}", &block);
 }
